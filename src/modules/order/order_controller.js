e082266a73eda3b1786a8ea4421f3e17b49c6861
elazsyahranie <elazaribrahims@gmail.com>
2022-08-27 17:15:36 +0700

    Implemented add order

--- src/modules/order/order_controller.js
+++ src/modules/order/order_controller.js
@@ -5,9 +5,17 @@ require('dotenv').config()
 module.exports = {
     addOrder: async (req, res) => {
         try {
-            console.log("Add order succesful!")
-        } catch {
-            console.log("Uuups")
+            const { menuId, customerId } = req.body 
+            const setData = {
+                menu_id: menuId, 
+                customer_id: customerId,
+            }
+            const { menu_id, customer_id } = setData; 
+            const result = await orderModel.addOrder(menu_id, customer_id) 
+            return helper.response(res, 200, 'Order succesful!', result)
+        } catch (error) {
+            console.log(error)
+            return helper.response(res, 400, 'Bad Request', error)
         }
     }
 }
--- src/modules/order/order_model.js
+++ src/modules/order/order_model.js
@@ -1,21 +1,28 @@
 const connection = require('../../config/mysql')  
 
 module.exports = {
-    addOrder: (menuId, customerId) => {
+    addOrder: (menu_id, customer_id) => {
         return new Promise((resolve, reject) => {
-            connection.query('INSERT INTO ordered_items SET ?', [menuId, customerId], 
-            (error, result) => {
-                if (!error) {
-                    const newResult = {
-                        id: result.insertId, 
-                        menu_id: menuId, 
-                        customer_id: customerId, 
+            // let query = 'START TRANSACTION'
+            const query = 'INSERT INTO ordered_items (menu_id, customer_id) VALUES ?'
+            // [items.map(item => [item.name, item.description, item.value])]
+            const values = [
+                [menu_id, customer_id]
+            ]
+            console.log(values)
+            connection.query(query, values, 
+                (error, result) => {
+                    if (!error) {
+                        const newResult = {
+                            id: result.insertId, 
+                            menu_id: menu_id, 
+                            customer_id: customer_id
+                        }
+                        resolve(newResult)
+                    } else {
+                        reject(new Error(error))
                     }
-                    resolve(newResult)
-                } else {
-                reject(new Error(error))
-                }
+                })
             })
-        })
-    }
+        }
 }
